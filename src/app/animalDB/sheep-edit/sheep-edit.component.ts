94265feebd559c5ce976f8c10b0399786bd2371f
Alexander <lucky-pa777@yandex.ru>
2021-04-10 21:16:33 +0300

    1. Просмотр всех животных 2. Редактирование животных (в процессе) 3. Рефакторинг (перенос интерфейсов в отдельную директорию interfaces) 4. Пропущены файлы sheep-edit.component

---
+++ src/app/animalDB/sheep-edit/sheep-edit.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Farm} from '../../interfaces/farm';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {Animal} from '../../interfaces/animal';
+
+@Component({
+  selector: 'app-sheep-edit',
+  templateUrl: './sheep-edit.component.html',
+  styleUrls: ['./sheep-edit.component.css']
+})
+export class SheepEditComponent implements OnInit {
+
+  constructor(private http: HttpClient,public route : ActivatedRoute, private router : Router) { }
+  public pending = false;
+  public AnimalInfo : Animal = {
+    id: '',
+    passport: {
+      chipNo: "123",
+
+    }
+  }
+  public form : FormGroup = new FormGroup({
+  })
+  ngOnInit(): void {
+    console.log(this.route.snapshot.queryParams);
+    //if (this.route.snapshot.params.)
+    this.route.queryParams.subscribe( params => {
+      console.log(params);
+      if (params.create === "false") {
+        this.pending = true
+        this.http.get("/api/sheep/"+this.route.snapshot.params.id).subscribe( (response:Animal) => {
+
+          const {_id,...rest} = response;
+          const obj : Animal = {...rest, id: response._id}
+          this.AnimalInfo = obj;
+
+          this.pending = false;
+
+        })
+      }
+
+    })
+  }
+  UpdateAnimal(farm :Farm ){
+    return this.http.patch(`http://localhost:3000/api/sheep/${farm.id}`,farm)
+  }
+  CreateAnimal(farm :Farm ){
+    return  this.http.post(`http://localhost:3000/api/sheep/`,farm)
+  }
+  Submit($event: Event) {
+    //$event.preventDefault();
+    this.pending=true;
+    const farm : Farm = { ... this.form.value }
+    let sub : Observable<any>;
+    if (this.route.snapshot.queryParams.create == "false") {
+      farm.id = this.route.snapshot.params.id;
+      sub= this.UpdateAnimal(farm);
+    } else {
+      sub=this.CreateAnimal(farm);
+    }
+    sub.subscribe(response => {
+      console.log(response);
+      this.pending=false;
+      this.router.navigate(['/journal/sheep'])
+    })
+  }
+}
